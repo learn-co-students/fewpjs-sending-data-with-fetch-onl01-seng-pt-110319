93dabdaf32b70543e13a79fcb5a3727420782572
Jordan <jordanleelong5@gmail.com>
2020-06-01 21:54:34 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+// A body with the name and email passed in as arguments to submitData.
+//  These should be assigned to name and email keys within an object. 
+//  This object should then be stringified.
+
+
+function submitData(userName, userEmail) {
+ 
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail})
+        })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(object){
+            document.body.innerHTML = object.id 
+        }) 
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+
+}
