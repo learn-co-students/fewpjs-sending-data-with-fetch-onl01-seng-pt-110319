2e168d51bf6708040ec4f8b3e1b6dd87d60d330b
antony_sanders <antony.devon92@gmail.com>
2020-06-05 02:16:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Add your code here
+function submitData(name, email) {
+    let data = { name, email };
+    let configObj = {
+        method: 'POST',
+        headers: {
+            "Content-type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch('http://localhost:3000/users', configObj)
+        .then(res => res.json())
+        .then(function(object) {
+            document.body.innerHTML = object["id"]
+        })
+        .catch(function(error) {
+            document.body.innerHTML = error.message
+        })
+}
